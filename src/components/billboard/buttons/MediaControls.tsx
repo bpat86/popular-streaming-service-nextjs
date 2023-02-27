fa4cea025a540a472e20ca01d08c7f3ae1d99fc1
Bobby Patterson <b.patterson0486@gmail.com>
2023-02-27 12:03:50 -0700

    refactor: typescript wip

--- src/components/billboard/buttons/MediaControls.tsx
+++ src/components/billboard/buttons/MediaControls.tsx
@@ -1,3 +1,4 @@
+import Tooltip from "@/components/tooltips/Tooltip";
 import clsxm from "@/lib/clsxm";
 import { MotionPathWrapper } from "@/lib/MotionPathWrapper";
 import { MotionSpanWrapper } from "@/lib/MotionSpanWrapper";
@@ -15,24 +16,29 @@ type MediaControlsProps = {
 };
 
 const AudioIcons = ({ isMuted }: { isMuted: boolean }) => {
-  return isMuted ? (
-    <>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z"
-        />
-      </svg>
-    </>
-  ) : (
+  // Show the muted icon if the video is muted
+  if (isMuted) {
+    return (
+      <>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z"
+          />
+        </svg>
+      </>
+    );
+  }
+  // Otherwise, show the unmuted icon
+  return (
     <>
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -115,6 +121,55 @@ const MediaControls = ({
   videoPlaying,
   videoCompleted,
 }: MediaControlsProps) => {
+  /**
+   * Handle the icons that are displayed in the media controls
+   */
+  function handleIcons() {
+    return (
+      <div className="absolute inset-0 flex items-center justify-center">
+        {videoPlaying ? (
+          inView ? (
+            <AudioIcons isMuted={isMuted} />
+          ) : (
+            <PausedIcon />
+          )
+        ) : videoCompleted ? (
+          <ReplayIcon />
+        ) : (
+          <PausedIcon />
+        )}
+      </div>
+    );
+  }
+
+  /**
+   * Handle the screen reader text that is displayed in the media controls
+   */
+  function handleDisplayText() {
+    return videoPlaying
+      ? inView
+        ? isMuted
+          ? "Mute"
+          : "Unmute"
+        : `${title} preview is paused`
+      : videoCompleted
+      ? `Replay ${title}`
+      : `${title} preview is paused`;
+  }
+
+  /**
+   * Handle the click event for the media controls
+   */
+  function handleClick() {
+    return videoPlaying
+      ? inView
+        ? toggleAudio()
+        : null
+      : videoCompleted
+      ? replayVideo()
+      : null;
+  }
+
   return (
     <MotionSpanWrapper
       initial={{ opacity: 0 }}
@@ -133,47 +188,19 @@ const MediaControls = ({
           ]
         )}
       >
-        <button
-          type="button"
-          className={clsxm(
-            "relative mx-1 flex h-8 w-8 items-center justify-center rounded-full border-2 border-white border-opacity-50 bg-transparent font-bold text-white transition duration-150 ease-out hover:border-opacity-100 hover:bg-white hover:bg-opacity-50 focus:border-opacity-100 focus:bg-white focus:bg-opacity-50 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white sm:h-9 sm:w-9 md:text-xl lg:h-10 lg:w-10 2xl:h-12 2xl:w-12",
-            [videoError && "hidden"]
-          )}
-          onClick={() =>
-            videoPlaying
-              ? inView
-                ? toggleAudio()
-                : null
-              : videoCompleted
-              ? replayVideo()
-              : null
-          }
-        >
-          <>
-            {videoPlaying ? (
-              inView ? (
-                <span className="sr-only">{isMuted ? "Mute" : "Unmute"}</span>
-              ) : (
-                <span className="sr-only">{title} is paused</span>
-              )
-            ) : (
-              <span className="sr-only">Replay {title}</span>
+        <Tooltip text={handleDisplayText()}>
+          <button
+            type="button"
+            className={clsxm(
+              "relative flex h-8 w-8 items-center justify-center rounded-full border-2 border-white border-opacity-50 bg-transparent font-bold text-white transition duration-150 ease-out hover:border-opacity-100 hover:bg-white hover:bg-opacity-50 focus:border-opacity-100 focus:bg-white focus:bg-opacity-50 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white sm:h-9 sm:w-9 md:text-xl lg:h-10 lg:w-10 2xl:h-12 2xl:w-12",
+              [videoError && "hidden"]
             )}
-          </>
-          <div className="absolute inset-0 flex items-center justify-center">
-            {videoPlaying ? (
-              inView ? (
-                <AudioIcons isMuted={isMuted} />
-              ) : (
-                <PausedIcon />
-              )
-            ) : videoCompleted ? (
-              <ReplayIcon />
-            ) : (
-              <PausedIcon />
-            )}
-          </div>
-        </button>
+            onClick={handleClick}
+          >
+            <span className="sr-only">{handleDisplayText()}</span>
+            {handleIcons()}
+          </button>
+        </Tooltip>
       </div>
     </MotionSpanWrapper>
   );
